af5710995d893bc52c4a38f1283afccc467d3787
hamza <hamza.hord@gmail.com>
2023-05-12 22:43:42 +0200

    test

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import BasketView from '../views/BasketView'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/basket',
+    name: 'basket',
+    component: BasketView
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/BasketView.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="about">
+      <h1>This is Basket page</h1>
+    </div>
+  </template>
+  
